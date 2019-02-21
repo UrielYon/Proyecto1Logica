53b175b121391d8984c49efc4a8914d288ef6d59
mauricioJSM <maw@ciencias.unam.mx>
2019-02-21 00:50:46 -0600

    Add files via upload

--- Semantics.hs
+++ Semantics.hs
@@ -1,5 +1,4 @@
-module Semantics 
-where
+module Semantics where
 
 import Syntax
 
@@ -10,54 +9,59 @@ inter e phi = case phi of
   TTrue -> True
   FFalse -> False
   V x -> elem x e
-  Neg p -> not ( p inter e p)
-  Disy p q -> (inter e p) && (inter e q)
+  Neg p -> not (inter e p)
+  Conj p q -> (inter e p) && (inter e q)
+  Disy p q -> (inter e p) || (inter e q)
   Imp p q -> not (inter e p) || (inter e q)
   Equiv p q -> (inter e p) == (inter e q)
 
 estados :: Prop -> [Estado]
 estados phi = subconj (vars phi)
 
--- 3. Conceptos semanticos
-
 modelos :: Prop -> [Estado]
-modelos phi = [e | e <- estados phi, interp phi]
+modelos phi = [e | e <- estados phi, (inter e phi) == True]
 
 tautologia :: Prop -> Bool
-tautologia :: phi = (modelos phi)
+tautologia phi = (modelos phi) == (estados phi)
 
 satisf :: Prop -> Bool
-satisf phi = modelos phi/= []
+satisf phi = modelos phi /= []
 
-insatisfen :: Estados -> Prop -> Bool
-insatisfen e phi = not ()
+insatisfen :: Estado -> Prop -> Bool
+insatisfen e phi = (inter e phi) == False
 --4
 
 equiv :: Prop -> Prop -> Bool
 equiv p q = tautologia (Equiv p q)
 
-contrad :: Prop -> Bool
-contrad phi = []
-
 --5 Consecuencia Logica
+{- 
 consecuencia :: [Prop] -> Prop -> Bool
-consecuancia gamma phi = instatisfConj(phi:gamma )             
---Auxiliares
 
+consecuencia gamma phi = insatisfen(phi:gamma) 
+-}
+
+--Auxiliares
 vars :: Prop -> [VarP]
 vars phi = case phi of
   TTrue -> []
   FFalse -> []
   V x -> [x]
-  Neg g -> vars p
+  Neg p -> vars p
   Conj p q -> union (vars p) (vars q)
   Disy p q -> union (vars p) (vars q)
   Imp p q -> union (vars p) (vars q)
   Equiv p q -> union (vars p) (vars q)
 
-  subconj :: [a] -> [[a]]
- -- subconj [] = [[]]
-  subconj (x:xs) = xs' ++ map  (x:) xs'
-   where xs' = subconj xs
+
+eliminaRep :: Eq a => [a] -> [a]
+eliminaRep [] = []
+eliminaRep (x:xs) = x : eliminaRep (filter (/=x) xs)
 
 
+union :: [VarP] -> [VarP] -> [VarP]
+union xs ys = eliminaRep (xs ++ ys)
+
+subconj :: [a] -> [[a]]
+subconj [] = [[]]
+subconj (x:xs) = xs' ++ map  (x:) xs' where xs' = subconj xs
--- Syntax.hs
+++ Syntax.hs
@@ -13,3 +13,63 @@ data Prop = TTrue
           | Imp Prop Prop
           | Equiv Prop Prop
 
+
+eliminaEq :: Prop -> Prop
+eliminaEq phi = case phi of
+	Neg p -> Neg(eliminaEq p)
+	Conj p q -> (Conj (eliminaEq p) (eliminaEq q))
+	Disy p q -> (Disy (eliminaEq p) (eliminaEq q))
+	Imp p q -> (Imp (eliminaEq p) (eliminaEq q))
+	Equiv p q -> (Conj (Imp p' q') (Imp p' q'))
+		where p' = eliminaEq p
+		      q' = eliminaEq q
+
+	_ -> phi
+
+eliminaImp :: Prop -> Prop
+eliminaImp phi = case phi of
+	Neg p -> Neg(eliminaImp p)
+	Conj p q -> (Conj (eliminaImp p) (eliminaImp q))
+	Disy p q -> (Disy (eliminaImp p) (eliminaImp q))
+	Imp p q -> (Disy (Neg (eliminaImp p)) (eliminaImp q))
+	Equiv p q -> (Equiv (eliminaImp p) (eliminaImp q))
+	_ -> phi
+
+meteNeg :: Prop -> Prop
+meteNeg phi = case phi of
+	TTrue -> TTrue
+	FFalse -> FFalse
+	V x -> V x
+	Neg psi -> case psi of
+		Conj p q -> Disy (meteNeg (Neg p)) (meteNeg(Neg q))
+		Disy p q -> Conj (meteNeg (Neg p)) (meteNeg (Neg q))
+		Neg p -> meteNeg p
+		TTrue -> FFalse
+		FFalse -> TTrue
+		V x -> Neg(V x)
+	Conj p q -> Conj (meteNeg p) (meteNeg q)
+	Disy p q -> Disy (meteNeg p) (meteNeg q)
+
+fnn :: Prop -> Prop
+fnn = meteNeg.eliminaImp.eliminaEq
+
+dist :: Prop -> Prop
+dist phi = case phi of
+	TTrue -> TTrue
+	FFalse -> FFalse
+	V x -> V x
+	Neg p -> Neg p
+	Conj p q -> Conj (dist p) (dist q)
+	Disy p (Conj q r) -> Conj (dist (Disy p q)) (dist (Disy p r))
+	Disy (Conj p q) r -> Conj (dist (Disy p r)) (dist (Disy q r))
+	Disy p q -> case (p' , q') of 
+		(Conj _ _, _) -> dist (Disy p' q')
+		(_, Conj _ _) -> dist (Disy p' q')
+		(_,_) -> Disy p' q'
+		where p' = dist p
+		      q' = dist q
+
+fnc :: Prop -> Prop
+fnc = dist.fnn
+
+		
